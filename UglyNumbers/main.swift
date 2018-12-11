f87a7671f0e2b038fceb42dc0f6468d45575751e
QueraltSosaMompel <queraltsosa@hotmail.com>
2018-12-11 22:07:32 +0000

    Initial Commit

---
+++ UglyNumbers/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  UglyNumbers
+//
+//  Created by Queralt Sosa Mompel on 11/12/18.
+//  Copyright © 2018 Queralt Sosa Mompel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
